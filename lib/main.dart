bc8d52ee40eb539e63c8f12b98f48ec50693b20c
Enzo <enzo.jolys@gmail.com>
2023-11-15 10:39:44 +0100

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:smartfit_app_mobile/modele/user.dart';
-import 'package:smartfit_app_mobile/view/login/signup_view.dart';
 import 'package:smartfit_app_mobile/common/colo_extension.dart';
+import 'package:smartfit_app_mobile/view/login/signup_view.dart';
 
 void main() {
   runApp(ChangeNotifierProvider(
