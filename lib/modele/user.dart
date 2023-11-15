64f6d5de0b264a3e5486c7b1c5c3fc89dc8b65d7
Enzo <enzo.jolys@gmail.com>
2023-11-15 14:09:52 +0100

    mv api + modele

--- lib/modele/user.dart
+++ lib/modele/user.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:smartfit_app_mobile/Modele/activity.dart';
 
-
-import 'package:flutter/material.dart';
-import 'package:smartfit_app_mobile/Modele/activity.dart';
-
 class User extends ChangeNotifier {
   String? username;
   String? email;
   String? token;
   List<ActivityOfUser> listActivity = [];
 
-
   void addActivity(ActivityOfUser activity) {
     listActivity.add(activity);
     notifyListeners();
-  }		  
+  }
 
   void removeActivity(ActivityOfUser activity) {
     listActivity.remove(activity);
