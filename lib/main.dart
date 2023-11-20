1c600440a576b9257afa33119bbb2825cec2c299
Enzo <enzo.jolys@gmail.com>
2023-11-20 16:02:58 +0100

    change name

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SmartFit 3 in 1',
+      title: 'SmartFit',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           // This is the theme of your application.
