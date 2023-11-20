b640a18bc50a0cf4cef2afcb529dfce11ccdccae
Enzo <enzo.jolys@gmail.com>
2023-11-20 15:45:13 +0100

    Home correction

--- lib/common_widget/container/workout_row.dart
+++ lib/common_widget/container/workout_row.dart
@@ -15,7 +15,6 @@ class WorkoutRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var media = MediaQuery.of(context).size;
     return Container(
       margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 2),
       padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
