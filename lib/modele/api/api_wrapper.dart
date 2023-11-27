d755378672075478e5e30b9074e0de1f1b18ea54
rem <remi.arnal@etu.uca.fr>
2023-11-27 14:04:31 +0100

    :sparkles: add support for password modification

--- lib/modele/api/api_wrapper.dart
+++ lib/modele/api/api_wrapper.dart
@@ -3,6 +3,8 @@ import 'package:smartfit_app_mobile/modele/api/request_api.dart';
 import 'package:smartfit_app_mobile/modele/utile/info_message.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:tuple/tuple.dart';
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
 
 class ApiWrapper {
   IDataStrategy api = RequestApi();
@@ -28,6 +30,21 @@ class ApiWrapper {
           "This is not a valid $infoToModify :/ Please try again.", true);
       return false;
     }
+  }
+
+  Future<Tuple2<bool, String>> login(
+      String password, String email, InfoMessage infoManager) async {
+    String hash = sha256.convert(utf8.encode(password)).toString();
+    Tuple2<bool, String> res = await api.connexion(email, hash);
+
+    if (res.item1) {
+      return Tuple2(true, res.item2); // return token
+    } else {
+      infoManager.displayMessage(
+          "Authentification failed! Enter your actual password carefully.",
+          true);
+      return const Tuple2(false, "An error occured during connexion!");
+    } // need to be better
   }
 }
 
